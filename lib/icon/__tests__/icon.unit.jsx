cd691c8c90830d01a7ccffc6de410917e2c5ffc5
dflk <904885508@qq.com>
2020-02-13 11:14:59 +0800

    使用enzyme测试点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Icon from '../../icon/icon';
+import {mount} from 'enzyme';
 
-describe('icon test', ()=>{
-    it('是个svg', ()=>{
+describe('icon test', () => {
+    it('是个svg', () => {
         const json = renderer.create(<Icon iconName="wechat"/>).toJSON();
-        expect(json).toMatchSnapshot()
-    })
-})
+        expect(json).toMatchSnapshot();
+    });
+
+    it('被点击', () => {
+        const fn = jest.fn();
+        const fn2 = jest.fn();
+        const component = mount(<Icon iconName="wechat" onClick={fn}/>);
+        component.find('svg').simulate('click');
+        expect(fn).toBeCalled();
+    });
+});
