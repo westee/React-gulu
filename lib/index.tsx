bfdf234082fb4007d7abbb4972a2912fbae7b069
dflk <904885508@qq.com>
2020-02-18 09:08:10 +0800

    配置自动化发布

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-const root = document.getElementById('root');
-const fn: React.MouseEventHandler = (e)=>{
-    console.log(e.target);
-};
-
-ReactDOM.render(
-    <div>
-        <Icon className="wechat" iconName='wechat' onClick={fn} onMouseEnter={fn} onMouseLeave={fn} />
-    </div>
-    , root);
+export  { default as Icon } from './icon/icon'
