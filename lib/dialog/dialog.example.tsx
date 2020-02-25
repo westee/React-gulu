97e5b3d3ad2f985f4aeec8ed654b4f6bc236c38d
westee <904885508@qq.com>
2020-02-25 21:42:39 +0800

    使用portal-api

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,18 +3,34 @@ import Dialog from "./dialog";
 
 export default function () {
     const [x, setX] = useState(false);
+    const [y, setY] = useState(false);
 
     return (
         <div>
-            <button onClick={()=>{setX(!x)}}>click</button>
-            <Dialog visible={x} buttons={
-                [
-                    <button onClick={()=>{setX(false)}}>1</button>,
-                    <button onClick={()=>{setX(false)}}>1</button>
-                ]
-            } onClose={()=>{setX(false)}} closeOnClickMask={true}>
-                <strong>nihao</strong>
-            </Dialog>
+            <div>
+                <button onClick={()=>{setX(!x)}}>click</button>
+                <Dialog visible={x} buttons={
+                    [
+                        <button onClick={()=>{setX(false)}}>1</button>,
+                        <button onClick={()=>{setX(false)}}>1</button>
+                    ]
+                } onClose={()=>{setX(false)}} closeOnClickMask={true}>
+                    <strong>nihao</strong>
+                </Dialog>
+            </div>
+
+            <div>
+                <button onClick={()=>{setX(!y)}}>click</button>
+                <Dialog visible={y} buttons={
+                    [
+                        <button onClick={()=>{setY(false)}}>1</button>,
+                        <button onClick={()=>{setY(false)}}>1</button>
+                    ]
+                } onClose={()=>{setY(false)}} closeOnClickMask={true}>
+                    <strong>nihao</strong>
+                </Dialog>
+            </div>
         </div>
+
     )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,6 +1,7 @@
 import React, {Fragment, ReactChild, ReactElement, ReactFragment, ReactPortal} from 'react';
 import './dialog.scss'
 import Icon from 'lib/icon/icon';
+import ReactDOM from 'react-dom';
 
 interface Props {
     visible: boolean;
@@ -28,28 +29,29 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             props.onClose(e);
         }
     }
-    return (
-        props.visible ?
-            <Fragment>
-                <div className={sc('mask')} onClick={onClickMask}/>
-                <div className={sc()}>
-                    <div className={sc('close')} onClick={onClickClose}>
-                        <Icon iconName={'close'}/>
-                    </div>
-                    <header className={sc('header')}>
-                        提示
-                    </header>
-                    <main className={sc('main')}>
-                        <div>{props.children}</div>
-                    </main>
-                    <footer className={sc('footer')}>
-                        {props.buttons.map((button, index) =>
-                            React.cloneElement(button, {key: index})
-                        )}
-                    </footer>
+    const x = props.visible ?
+        <Fragment>
+            <div className={sc('mask')} onClick={onClickMask}/>
+            <div className={sc()}>
+                <div className={sc('close')} onClick={onClickClose}>
+                    <Icon iconName={'close'}/>
                 </div>
-            </Fragment>
-            : null
+                <header className={sc('header')}>
+                    提示
+                </header>
+                <main className={sc('main')}>
+                    <div>{props.children}</div>
+                </main>
+                <footer className={sc('footer')}>
+                    {props.buttons.map((button, index) =>
+                        React.cloneElement(button, {key: index})
+                    )}
+                </footer>
+            </div>
+        </Fragment>
+        : null
+    return (
+        ReactDOM.createPortal(x, document.body)
     )
 }
 
