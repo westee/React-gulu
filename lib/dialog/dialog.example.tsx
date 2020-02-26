c4b55fbaf65916be36939bd33e57a1d10d540de0
westee <904885508@qq.com>
2020-02-26 22:48:36 +0800

    为dialog添加confirm和modal api

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,36 +1,64 @@
 import React, {useState} from "react";
-import Dialog, {alert} from "./dialog";
+import Dialog, {alert, confirm, modal} from "./dialog";
 
 export default function () {
     const [x, setX] = useState(false);
     const [y, setY] = useState(false);
+    const openModal = ()=>{
+        const close = modal((<h1>爱你哟 <button onClick={()=>close()}>close</button></h1>))
+    }
 
     return (
         <div>
             <div>
-                <button onClick={()=>{setX(!x)}}>click</button>
+                <button onClick={() => {
+                    setX(!x)
+                }}>click
+                </button>
                 <Dialog visible={x} buttons={
                     [
-                        <button onClick={()=>{setX(false)}}>1</button>,
-                        <button onClick={()=>{setX(false)}}>1</button>
+                        <button onClick={() => {
+                            setX(false)
+                        }}>1</button>,
+                        <button onClick={() => {
+                            setX(false)
+                        }}>1</button>
                     ]
-                } onClose={()=>{setX(false)}} closeOnClickMask={true}>
+                } onClose={() => {
+                    setX(false)
+                }} closeOnClickMask={true}>
                     <strong>nihao</strong>
                 </Dialog>
             </div>
 
             <div>
-                <button onClick={()=>{setX(!y)}}>click</button>
+                <button onClick={() => {
+                    setX(!y)
+                }}>click
+                </button>
                 <Dialog visible={y} buttons={
                     [
-                        <button onClick={()=>{setY(false)}}>1</button>,
-                        <button onClick={()=>{setY(false)}}>1</button>
+                        <button onClick={() => {
+                            setY(false)
+                        }}>1</button>,
+                        <button onClick={() => {
+                            setY(false)
+                        }}>1</button>
                     ]
-                } onClose={()=>{setY(false)}} closeOnClickMask={true}>
+                } onClose={() => {
+                    setY(false)
+                }} closeOnClickMask={true}>
                     <strong>nihao</strong>
                 </Dialog>
             </div>
-            <div onClick={()=>alert('1')}>alert</div>
+            <div onClick={() => alert('1')}>alert</div>
+            <div onClick={() => confirm('2', () => {
+                console.log('yes')
+            }, () => {
+                console.log('no')
+            })}>confirm
+            </div>
+            <div onClick={openModal}>modal</div>
         </div>
 
     )
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, ReactChild, ReactElement, ReactFragment, ReactPortal} from 'react';
+import React, { Fragment, ReactChild, ReactElement, ReactFragment, ReactPortal} from 'react';
 import './dialog.scss'
 import Icon from 'lib/icon/icon';
 import ReactDOM from 'react-dom';
@@ -24,8 +24,8 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     const onClickClose: React.MouseEventHandler = (e) => {
         props.onClose(e);
     };
-    const onClickMask: React.MouseEventHandler = e=>{
-        if(props.closeOnClickMask){
+    const onClickMask: React.MouseEventHandler = e => {
+        if (props.closeOnClickMask) {
             props.onClose(e);
         }
     }
@@ -56,16 +56,62 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
 }
 
 Dialog.defaultProps = {closeOnClickMask: false}
-const alert = (content: string ) =>{
-    const component = <Dialog visible={true} onClose={()=>{
+const alert = (content: string) => {
+    const onClose = () => {
         ReactDOM.render(React.cloneElement(component, {visible: false}), div);
         ReactDOM.unmountComponentAtNode(div);
         div.remove();
-    }}>我是内容</Dialog>;
+    }
+    const component = <Dialog visible={true} onClose={onClose}>我是内容</Dialog>;
     const div = document.createElement('div');
     document.body.append(div);
     ReactDOM.render(component, div);
 }
-export {alert};
+
+const confirm = (content: string, yes?: () => void, no?: () => void) => {
+    const onYes = () => {
+        ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+        yes && yes();
+    };
+    const onNo = () => {
+        ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+        no && no();
+    };
+    const component = (
+        <Dialog
+            onClose={onNo}
+            visible={true}
+            buttons={[<button onClick={onYes}>yes</button>, <button onClick={onNo}>no</button>]}
+        >
+            {content}
+        </Dialog>);
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    ReactDOM.render(component, div);
+}
+
+const modal = (content:  ReactFragment | ReactElement) => {
+    const onClose = ()=>{
+        ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+    }
+    const component = (
+        <Dialog
+            visible={true}
+            onClose={onClose}
+        >
+            {content}
+        </Dialog>);
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    ReactDOM.render(component, div);
+    return onClose;
+}
+export {alert, confirm, modal};
 
 export default Dialog;
