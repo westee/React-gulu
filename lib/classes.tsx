f0854ac3fc7ffd422bf5afab12f89013bf0c0e1f
westee <904885508@qq.com>
2020-03-02 21:30:03 +0800

    修改classes.tsx的功能

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -1,5 +1,15 @@
+interface extraClass {
+    extra: string | undefined
+}
+
 export default function scopedClassName(preName: string) {
-    return function f1(name?: string) {
-        return [preName, name].filter(Boolean).join('-');
+    return function f1(name?: string,  option?: extraClass) {
+        const result = [preName, name].filter(Boolean).join('-');
+
+        if(option && option.extra){
+            return [result, option && option.extra].filter(Boolean).join(' ');
+        } else {
+            return result;
+        }
     }
 }
